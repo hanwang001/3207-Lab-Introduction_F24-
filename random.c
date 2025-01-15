43ab7e79f0b5e2a8bd5ce6669d024064e91754ee
Han Wang <150043566+hanwang001@users.noreply.github.com>
2025-01-15 14:08:52 -0500

    Update random.c

--- random.c
+++ random.c
@@ -1,1 +1,8 @@
+#include <stdlib.h>
+
+/* Function to return a random uppercase character from 'A' to 'Z' */
+char randchar()
+{
+    return 'A' + (rand() % 26);  // Generates a random letter between A and Z.
+}
 
